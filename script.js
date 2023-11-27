87664e795b2963eab9d15ab5581945e105e54396
PrasadKale18 <prasadkale932@gmail.com>
2023-11-27 19:44:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = document.querySelector("#container");
+
+let heart = document.querySelector("i");
+
+a.addEventListener("dblclick",function(){
+    heart.style.transform = "translate(-50%,-50%) scale(1)";
+    heart.style.opacity = 0.8;
+    setTimeout(function(){
+        heart.style.opacity = 0;
+    },1000);
+
+    setTimeout(function(){
+        heart.style.transform = "translate(-50%,-50%) scale(0)";
+    },2000);
+})
